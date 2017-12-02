a3f6f0d6ecf8d790482880818a614d962acdac10
Stewart L. Mccready <stewmcc@gmail.com>
2017-12-02 02:31:59 +0000

    Adds Dampening to the follow camera on LAD
    Former-commit-id: 439e64284005b138aa6cc330dfc340228e2339f0

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,78 +6,83 @@
 /// </summary>
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
-	[SerializeField]
-	private RobotController robot =null;
+    [SerializeField]
+    private RobotController robot = null;
+    
+    [SerializeField]
+    private float followSpeed = 2.0f;
 
-	[Tooltip("Distance camera is offset from the bot.")]
-	[SerializeField]
-	private Vector3 cameraOffset = Vector3.zero;
+    [Tooltip("Distance camera is offset from the bot.")]
+    [SerializeField]
+    private Vector3 cameraOffset = Vector3.zero;
 
-	[SerializeField]
-	private float transitionTime = 3;
+    [SerializeField]
+    private float logoTransitionTime = 3;
 
-	private float startTime_ =0;
+    private float startTime_ = 0;
 
-	private Vector3 initialPosition_ = Vector3.zero;
+    private Vector3 initialPosition_ = Vector3.zero;
 
-	private Vector3 lerpStartPosition_ = Vector3.zero;
+    private Vector3 lerpStartPosition_ = Vector3.zero;
 
-	private bool isFollowingRobot_ = true;
+    private bool isFollowingRobot_ = true;
 
-	private bool isMovingToDistressBeacon_ = true;
+    private bool isMovingToDistressBeacon_ = true;
 
-	private void Start() {
-		initialPosition_ = gameObject.transform.position;
-	}
+    private void Start() {
+        initialPosition_ = gameObject.transform.position;
+        transform.position = new Vector3(
+            robot.transform.position.x + cameraOffset.x,
+            transform.position.y + cameraOffset.y,
+            robot.transform.position.z + cameraOffset.z);
+    }
 
-	private void OnEnable() {
-		EventManager.OnShipFixed += StartMovingToDistressBeacon;
-	}
+    private void OnEnable() {
+        EventManager.OnShipFixed += StartMovingToDistressBeacon;
+    }
 
-	private void OnDisable() {
-		EventManager.OnShipFixed -= StartMovingToDistressBeacon;
-	}
+    private void OnDisable() {
+        EventManager.OnShipFixed -= StartMovingToDistressBeacon;
+    }
 
-	/// <summary>
-	/// Just before rendering, updates the camera to follow the robot.
-	/// </summary>
-	private void LateUpdate() {
-		if (isFollowingRobot_) {
+    /// <summary>
+    /// Just before rendering, updates the camera to follow the robot.
+    /// </summary>
+    private void LateUpdate() {
+        if (isFollowingRobot_) {
 
-			transform.position = new Vector3(
-				robot.transform.position.x + cameraOffset.x,
-				transform.position.y + cameraOffset.y,
-				robot.transform.position.z + cameraOffset.z);
+            // lerped animation between 2 points.
+            transform.position = Vector3.Lerp(transform.position, 
+                new Vector3(
+                robot.transform.position.x + cameraOffset.x,
+                transform.position.y + cameraOffset.y,
+                robot.transform.position.z + cameraOffset.z), 
+                Time.deltaTime * followSpeed
+            );
+        }
+        else if (isMovingToDistressBeacon_) {
+            transform.position = MathUtil.SmoothLerp(
+                lerpStartPosition_,
+                initialPosition_,
+                startTime_,
+                logoTransitionTime,
+                out isMovingToDistressBeacon_);
 
+            if (!isMovingToDistressBeacon_) {
+                EventManager.CameraInPosition();
+            }
+        }
+    }
 
-		}
-		else if (isMovingToDistressBeacon_) {
-			transform.position = MathUtil.SmoothLerp(
-				lerpStartPosition_,
-				initialPosition_,
-				startTime_,
-				transitionTime,
-				out isMovingToDistressBeacon_);
+    /// <summary>
+    /// Start the camera moving towards the distress beacon at its initial position.
+    /// When the ship has been fixed .
+    /// </summary>
+    private void StartMovingToDistressBeacon() {
+        isFollowingRobot_ = false;
+        isMovingToDistressBeacon_ = true;
 
-			if (!isMovingToDistressBeacon_) {
-				EventManager.CameraInPosition();
-			}
-		}
-	}
-
-	/// <summary>
-	/// Start the camera moving towards the distress beacon at its initial position.
-	/// When the ship has been fixed .
-	/// </summary>
-	private void StartMovingToDistressBeacon() {
-		isFollowingRobot_ = false;
-		isMovingToDistressBeacon_ = true;
-
-		startTime_ = Time.time;
-		lerpStartPosition_ = gameObject.transform.position;
-	}
-
-
-
-
+        startTime_ = Time.time;
+        lerpStartPosition_ = gameObject.transform.position;
+    }
 }
