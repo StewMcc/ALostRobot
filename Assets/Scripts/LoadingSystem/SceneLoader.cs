f85307aaa3d55bca952581eef6d4c83ca2a00223
Stewart L. Mccready <stewmcc@gmail.com>
2017-12-08 23:05:00 +0000

    Seperate debug Ui code so easier to reuse in other scenes.

---
+++ Assets/Scripts/LoadingSystem/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Scene loader will load the provided scene.
+/// 
+/// Will use basic async if no transition controller or use Loading Scene not set.
+/// Otherwise uses loading screen Setup.
+/// </summary>
+public class SceneLoader : MonoBehaviour {
+
+	[SerializeField]
+	private string nextScene = "nextScene NotSet";
+	[SerializeField]
+	private string loadingScene = "loadingScreen NotSet";
+
+	/// <summary>
+	/// Loads the scenes set on the object.
+	/// </summary>
+	public void LoadScene() {
+		LoadingTransitionController.AnimatedLoadSceneAsync(nextScene, loadingScene);
+	}
+}
