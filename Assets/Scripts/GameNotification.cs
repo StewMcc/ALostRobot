2d4d4bab3a0f841ca36759e9cf77ff0d8e6cd09c
Stewart L. Mccready <stewmcc@gmail.com>
2018-01-08 05:43:55 +0000

    Adds In-Game notifications for discovering items.
    Former-commit-id: 0680ff2c46fdd6ea9a5150d55849115eb97c8103

---
+++ Assets/Scripts/GameNotification.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameNotification : MonoBehaviour {
+
+	[SerializeField]
+	CanvasGroup notificationGroup;
+
+	[SerializeField]
+	Image iconImage;
+
+	[SerializeField]
+	Text title;
+
+	Vector3 initialPosition = new Vector3();
+
+	float timeCreated = 0;
+
+	bool isActive = false;
+
+	public bool IsActive() {
+		return isActive;
+	}
+
+	public void InitGameNotification(GameNotificationData data, float timeNotificationCreated) {
+		iconImage.sprite = data.iconSprite;
+		title.text = data.notificationTitle;
+		notificationGroup.alpha = 0.0f;
+		transform.position = initialPosition;
+		timeCreated = timeNotificationCreated;
+		isActive = true;
+	}
+
+	public void FadeOut(float currentTime, float fadeInTime, float showTime, float fadeOutTime) {
+		float timeAlive = currentTime - timeCreated;
+
+		if (timeAlive > (fadeInTime + showTime)) {
+
+			float percentageComplete = (timeAlive - (fadeInTime + showTime)) / fadeOutTime;
+			notificationGroup.alpha = 1.0f - percentageComplete;
+
+			if (percentageComplete > 1.0f) {
+				isActive = false;
+			}
+		} else {
+			float percentageComplete = timeAlive / fadeOutTime;
+			notificationGroup.alpha = percentageComplete;
+		}
+	}
+
+	private void Start() {
+		initialPosition = transform.position;
+		notificationGroup.alpha = 0;
+	}
+
+}
