bc579e598a8324568918edc4dc75f761246e7b92
Stewart L. Mccready <stewmcc@gmail.com>
2018-01-08 20:03:55 +0000

    Add Notification when rooms broken
    Tidies up formating/comments aswell. Still need to fix room breaking duplicate rooms.
    
    Former-commit-id: e748911ad7c8fef15f9c6729cb27807480919ef1

--- Assets/Scripts/Debug/DebugNotifications.cs
+++ Assets/Scripts/Debug/DebugNotifications.cs
@@ -7,7 +7,15 @@ public class DebugNotifications : MonoBehaviour {
 	[SerializeField]
 	GameNotificationData notification;
 
+	private int notificationNumber_ = 0;
+
 	public void SendTestNotification() {
-		GameNotificationHandler.SendGameNotification(notification);
+		notificationNumber_++;
+		GameNotificationData data;
+		data.notificationTitle = notificationNumber_ + " " + notification.notificationTitle;
+		data.iconSprite = notification.iconSprite;
+
+		GameNotificationHandler.SendGameNotification(data);
 	}
+
 }
